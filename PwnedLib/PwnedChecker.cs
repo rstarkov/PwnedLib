f632f591d1311a481bff030386e87e167c8ea828
rstarkov <sourcecode@roman.st>
2018-04-27 23:18:26 +0100

    Opening and closing the file takes up a large percentage of the CPU when running off an SSD, so the checker will now keep the file open until disposed of.

--- PwnedLib/PwnedChecker.cs
+++ PwnedLib/PwnedChecker.cs
@@ -6,17 +6,20 @@ using System.Text;
 namespace PwnedLib
 {
     /// <summary>
-    ///     Implements methods to look up passwords in the Have I Been Pwned database. Every method of this class is
-    ///     thread-safe.</summary>
-    public class PwnedChecker
+    ///     Implements methods to look up passwords in the Have I Been Pwned database. This class is not thread-safe, but
+    ///     multiple instances can safely operate in parallel on the same database file.</summary>
+    public class PwnedChecker : IDisposable
     {
+        private Stream _stream;
+        private BinaryReader2 _reader;
+
         /// <summary>Gets the file name of the HIBP database.</summary>
         public string DbFileName { get; private set; }
 
         /// <summary>
-        ///     Constructor. Ensures that the specified database file exists. Creating instances of this class is cheap. The
-        ///     database file is only opened briefly during each check. It doesn't matter whether you create a new instance
-        ///     for each check, or initialise a single instance for later reuse.</summary>
+        ///     Constructor. Ensures that the specified database file exists. The database file remains open until this
+        ///     instance is disposed of. Creating instances of this class is cheap, but reusing an instance when performing
+        ///     large numbers of lookups will improve performance.</summary>
         /// <param name="dbFileName">
         ///     The name of the HIBP database file. The format of this file is specific to PwnedLib; use PwnedGen to generate
         ///     one. See documentation on the project website.</param>
@@ -25,6 +28,17 @@ namespace PwnedLib
             DbFileName = dbFileName;
             if (!File.Exists(dbFileName))
                 throw new ArgumentException($"The specified file does not exist: \"{dbFileName}\"", "dbFileName");
+            _stream = File.Open(DbFileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            _reader = new BinaryReader2(_stream);
+        }
+
+        /// <summary>Disposes of this class and closes the database file.</summary>
+        public void Dispose()
+        {
+            _stream?.Dispose();
+            _reader?.Dispose();
+            _stream = null;
+            _reader = null;
         }
 
         /// <summary>
@@ -75,56 +89,54 @@ namespace PwnedLib
                 throw new ArgumentNullException("hash");
             if (hash.Length != 20)
                 throw new ArgumentException("The hash is expected to be 20 bytes long.", "hash");
+            if (_stream == null || _reader == null)
+                throw new ObjectDisposedException(nameof(PwnedChecker));
 
-            using (var stream = File.Open(DbFileName, FileMode.Open, FileAccess.Read, FileShare.Read))
-            using (var reader = new BinaryReader2(stream))
+            int prefix = (hash[0] << 16) + (hash[1] << 8) + hash[2];
+            _stream.Position = prefix * 5;
+            long pos = _reader.ReadUInt32();
+            pos += (long) _reader.ReadByte() << 32;
+            if (pos == 0)
+                return 0;
+            _stream.Position = pos;
+
+            int entryCount = _reader.Read7BitEncodedInt();
+            if (entryCount == 0)
+                return 0;
+
+            var bytes = _reader.ReadBytes(entryCount * 17);
+            int min = 0;
+            int max = entryCount - 1;
+            int cur = 0;
+            while (true)
             {
-                int prefix = (hash[0] << 16) + (hash[1] << 8) + hash[2];
-                stream.Position = prefix * 5;
-                long pos = reader.ReadUInt32();
-                pos += (long) reader.ReadByte() << 32;
-                if (pos == 0)
-                    return 0;
-                stream.Position = pos;
-
-                int entryCount = reader.Read7BitEncodedInt();
-                if (entryCount == 0)
-                    return 0;
-
-                var bytes = reader.ReadBytes(entryCount * 17);
-                int min = 0;
-                int max = entryCount - 1;
-                int cur = 0;
-                while (true)
+                cur = (min + max) / 2;
+                for (int i = 0; i < 17; i++)
                 {
-                    cur = (min + max) / 2;
-                    for (int i = 0; i < 17; i++)
+                    int cmp = hash[3 + i] - bytes[cur * 17 + i];
+                    if (cmp < 0)
                     {
-                        int cmp = hash[3 + i] - bytes[cur * 17 + i];
-                        if (cmp < 0)
-                        {
-                            max = cur - 1;
-                            goto different;
-                        }
-                        else if (cmp > 0)
-                        {
-                            min = cur + 1;
-                            goto different;
-                        }
+                        max = cur - 1;
+                        goto different;
                     }
-                    break; // match found; its index is in "cur"
-                    different:
-                    if (min > max)
-                        return 0;
+                    else if (cmp > 0)
+                    {
+                        min = cur + 1;
+                        goto different;
+                    }
                 }
-
-                if (!needCount)
-                    return 1;
-
-                for (int i = 0; i < cur; i++)
-                    reader.Read7BitEncodedInt();
-                return reader.Read7BitEncodedInt();
+                break; // match found; its index is in "cur"
+                different:
+                if (min > max)
+                    return 0;
             }
+
+            if (!needCount)
+                return 1;
+
+            for (int i = 0; i < cur; i++)
+                _reader.Read7BitEncodedInt();
+            return _reader.Read7BitEncodedInt();
         }
     }
 
